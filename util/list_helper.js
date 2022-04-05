595c2adb421e780dcec2be483ce3fb26d93df42a
emmanz95 <emmanza2@gmail.com>
2022-04-05 23:44:26 +0100

    started exercises 4.3-4.7

---
+++ util/list_helper.js
@@ -0,0 +1,11 @@
+
+const dummy = (blogs) => {
+    return 1
+}
+
+const totalLikes = (blogs) => {
+    return blogs.length === 0 ?
+        0 : blogs.reduce((sum, blogs) => sum + blogs.likes, 0)
+}
+
+module.exports = { dummy, totalLikes }
